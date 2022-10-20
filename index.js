c458cd7da9bb90e6b7445e7fe7f7bc63742966e9
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-19 21:14:35 -0400

    put port in env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 5000
+const port = process.env.PORT
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
