3cd65324f806734c4c6563578c1f490c19d96d3f
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-22 21:45:55 -0400

    prettify

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import dotenv from 'dotenv'
-import cors from 'cors'
-import express from 'express'
-import bodyParser from 'body-parser'
-import { getUsers, addUser, setKiteSellability } from "./testPrisma.js";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { getUsers, addUser, setKiteSellability } from './testPrisma.js';
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-	res.send(await getUsers())
-})
+    res.send(await getUsers());
+});
 
 app.get('/hi', (req, res) => {
-	res.send('Hi from yechiel')
-})
+    res.send('Hi from yechiel');
+});
 
 app.post('/hi', (req, res) => {
-	res.send(`Hi from ${req.body.hifrom}`)
-})
+    res.send(`Hi from ${req.body.hifrom}`);
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
--- testPrisma.js
+++ testPrisma.js
@@ -1,51 +1,51 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getUsers = async () => {
-  const allUsers = await prisma.user.findMany({
-    include: {
-      kites: true,
-    },
-  });
-  return allUsers;
+    const allUsers = await prisma.user.findMany({
+        include: {
+            kites: true
+        }
+    });
+    return allUsers;
 };
 
 export const addUser = async (
-  name,
-  email,
-  kitePrice,
-  kiteBrand,
-  kiteModel,
-  kiteYear,
-  kiteSize,
-  kiteCountry,
-  kiteCity,
-  kiteAdress
+    name,
+    email,
+    kitePrice,
+    kiteBrand,
+    kiteModel,
+    kiteYear,
+    kiteSize,
+    kiteCountry,
+    kiteCity,
+    kiteAdress
 ) => {
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      kites: {
-        create: {
-          price: kitePrice,
-          brand: kiteBrand,
-          model: kiteModel,
-          year: kiteYear,
-          size: kiteSize,
-          country: kiteCountry,
-          city: kiteCity,
-          address: kiteAdress,
-        },
-      },
-    },
-  });
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            kites: {
+                create: {
+                    price: kitePrice,
+                    brand: kiteBrand,
+                    model: kiteModel,
+                    year: kiteYear,
+                    size: kiteSize,
+                    country: kiteCountry,
+                    city: kiteCity,
+                    address: kiteAdress
+                }
+            }
+        }
+    });
 };
 
 export const setKiteSellability = async (id, onSale) => {
-  const kite = await prisma.kite.update({
-    where: { id },
-    data: { onSale },
-  });
-  return kite;
+    const kite = await prisma.kite.update({
+        where: { id },
+        data: { onSale }
+    });
+    return kite;
 };
