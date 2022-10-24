0263f39fb8f9c6260ae65b31673aaf9de9fff58b
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-24 00:06:02 -0400

    reconnect database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
-// import { getUsers, addUser, addKite, setKiteSellability, deleteUser, deleteKite, getKites } from './testPrisma.js';
+import { getUsers, addUser, addKite, setKiteSellability, deleteUser, deleteKite, getKites } from './testPrisma.js';
 
 dotenv.config();
 const app = express();
@@ -15,9 +15,9 @@ app.get('/', (req, res) => {
     res.send('Hi Ta! from Yechiel');
 });
 
-// app.get('/kites', async (req, res) => {
-//     res.send(await getKites());
-// });
+app.get('/kites', async (req, res) => {
+    res.send(await getKites());
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
